436f5399b5ffa5cb9f8badc1d5840951cfde3114
Nils Schweinsberg <mail@n-sch.de>
2009-11-11 23:06:11 +0100

    testing

--- src/Paste/Control.hs
+++ src/Paste/Control.hs
@@ -7,11 +7,17 @@ import Control.Monad (msum)
 
 import Paste.View (showPaste)
 import Paste.Post (postHandler)
+import Paste.State (GetNewId(..))
 
 pasteHandler :: ServerPartT IO Response
 pasteHandler = msum
     [ postHandler
     , dir "static" $ fileServe [] "npaste.de"
+    , dir "show" $ showInformation
     , path showPaste
     , fileServe ["index.html"] "npaste.de"
     ]
+
+showInformation = do
+    paste <- query $ GetNewId
+    ok . toResponse $ show paste
