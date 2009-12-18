c5765c15657a1c3243b8ae43e59fc036eee9ec87
Nils Schweinsberg <mail@n-sch.de>
2009-12-18 16:59:55 +0100

    PasteEntry von 3 auf 4 migration vergessen…

--- src/Paste/State/Old/PasteEntry3.hs
+++ src/Paste/State/Old/PasteEntry3.hs
@@ -3,7 +3,7 @@
     TypeSynonymInstances, UndecidableInstances
     #-}
 
-module Paste.State.PasteEntry ( PasteEntry (..) ) where
+module Paste.State.Old.PasteEntry3 ( PasteEntry (..) ) where
 
 import Happstack.Data
 import Happstack.Server.HTTP.Types      (Host)
@@ -14,28 +14,27 @@ import Paste.State.ID                   (ID (..))
 import Paste.State.Content              (Content (..))
 
 import qualified Data.ByteString as BS
-import qualified Paste.State.Old.PasteEntry3 as Old
+import qualified Paste.State.Old.PasteEntry2 as Old
 
 $(deriveAll [''Show, ''Eq, ''Ord, ''Default]
     [d|
 
         -- | PasteEntry: Simple paste entry
         data PasteEntry = PasteEntry
-                    { user        :: Maybe User
-                    , pId         :: ID
-                    , date        :: ClockTime
-                    , content     :: Content
-                    , md5hash     :: BS.ByteString
-                    , filetype    :: Maybe String
-                    , postedBy    :: Host
-                    , description :: Maybe String
+                    { user      :: Maybe User
+                    , pId       :: ID
+                    , date      :: ClockTime
+                    , content   :: Content
+                    , md5hash   :: BS.ByteString
+                    , filetype  :: Maybe String
+                    , postedBy  :: Host
                     }
     |])
 
 $(deriveSerialize ''PasteEntry)
 instance Version PasteEntry where
     -- mode = Versioned 2 Nothing
-    mode = extension 4 (Proxy :: Proxy Old.PasteEntry)
+    mode = extension 3 (Proxy :: Proxy Old.PasteEntry)
 
 instance Migrate Old.PasteEntry PasteEntry where
-    migrate (Old.PasteEntry u i d c md f h) = PasteEntry u i d c md f h Nothing
+    migrate (Old.PasteEntry u i d c md f) = PasteEntry u i d c md f ("", 0)
