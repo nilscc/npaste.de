f5725b7374dfbdf48521ed9639a7f989c9a55871
Nils Schweinsberg <mail@n-sch.de>
2010-01-18 01:22:41 +0100

    desc parser verbessert

--- src/Paste/Parser/Description.hs
+++ src/Paste/Parser/Description.hs
@@ -3,8 +3,21 @@ module Paste.Parser.Description
     , DescVal (..)
     ) where
 
-import Text.ParserCombinators.Parsec
+import Control.Applicative
+import Control.Monad (MonadPlus(..), ap)
+import Text.ParserCombinators.Parsec hiding (many, optional, (<|>))
 
+-- Every Monad is an Applicative.
+instance Applicative (GenParser s a) where
+    pure = return
+    (<*>) = ap
+
+-- Every MonadPlus is an Alternative.
+instance Alternative (GenParser s a) where
+    empty = mzero
+    (<|>) = mplus
+
+-- Our data structure
 data DescVal = ID String
              | Username String
              | Tag String
@@ -20,30 +33,16 @@ parseDesc = either (const []) id . parse parseAll "description"
 -- parse different DescVals:
 pasteId, userName, tag, descVal :: Parser DescVal
 
-pasteId = try $ do
-    char '/'
-    id <- many1 alphaNum
-    char '/'
-    return $ ID id
-
-userName = try $ do
-    char '@'
-    un <- many1 alphaNum
-    return $ Username un
-
-tag = try $ do
-    char '#'
-    t <- many1 alphaNum
-    return $ Tag t
+pasteId  = try $ char '/' *> ID       `fmap` many1 alphaNum <* char '/'
+userName = try $ char '@' *> Username `fmap` many1 alphaNum
+tag      = try $ char '#' *> Tag      `fmap` many1 alphaNum
 
 descVal = pasteId <|> userName <|> tag
 
-
 -- put everything together
 parseAll :: Parser [DescVal]
-parseAll = do
-    val  <- descVal <|> (anyChar >>= return . Text . (:[]))
-    rest <- parseAll <|> return []
-    return $ case val : rest of
-                  (Text a : Text b : rest) -> Text (a++b) : rest
-                  other -> other
+parseAll = concatText <$> (descVal  <|> Text `fmap` pure `fmap` anyChar)
+                      <*> (parseAll <|> return [])
+
+  where concatText (Text a) (Text b : rest) = Text (a++b) : rest
+        concatText a rest = a : rest
