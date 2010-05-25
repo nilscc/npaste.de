ff628a8a2acdb21f4ee62028ca2df969226f5615
Nils Schweinsberg <mail@n-sch.de>
2010-05-25 12:31:18 +0200

    Client commandline: username/password
    TODO: Save username/password to config file and use this by default.

--- client/Np.hs
+++ client/Np.hs
@@ -15,25 +15,44 @@ import System (getProgName, exitWith, ExitCode(..))
 import System.Console.GetOpt
 import System.Directory (doesFileExist)
 import System.FilePath (takeExtension, extSeparator)
-import System.IO (hPutStrLn, stderr)
+import System.IO (hPutStrLn, stderr, stdout, hFlush)
 import System.Environment (getArgs)
+
+
 
 main = do
     args <- getArgs
 
     -- run getOpt
     let (actions, nonOptions, error) = getOpt RequireOrder options args
-    opts <- foldl' (>>=) (return $ Options Nothing Nothing Nothing []) actions
+    opts <- foldl' (>>=) (return $ Options Nothing Nothing Nothing [] Nothing Nothing) actions
 
-    let id = pId opts
-        file = case nonOptions of
+    let file = case nonOptions of
                     (file:_) -> file
                     _ -> ""
+
         ft = case filetype opts of
                   Just f -> f
                   Nothing -> case takeExtension file of
                                   (extSeparator : ext) -> ext
-                                  ext -> ext
+                                  ext                  -> ext
+
+        desc = case replies opts of
+                    []   -> description opts
+                    list -> Just $ "Reply to "
+                                    ++ (foldr (\id rest -> "/" ++ id ++ "/" ++ rest) "" list)
+                                    ++ (maybe "" (". " ++) $ description opts)
+
+        opts' = opts { filetype = Just ft, description = desc }
+
+    -- check for username/password
+    opts'' <- case opts' of
+                  Options { user = Just un, password = Nothing} -> do
+                      putStrLn $ "Enter password for username '" ++ un ++ "':"
+                      hFlush stdout
+                      pw <- getLine
+                      return opts' { password = Just pw }
+                  _ -> do return opts'
 
     -- get file content/read from stdin
     exists <- doesFileExist file
@@ -42,23 +61,23 @@ main = do
                else getContents
 
     -- post it! combine replies + description
-    post ft id text $ case replies opts of
-                           []   -> description opts
-                           list -> Just $ "Reply to "
-                                        ++ (foldr (\id rest -> "/" ++ id ++ "/" ++ rest) "" list)
-                                        ++ (maybe "" (". " ++) $ description opts)
+    post opts'' text
     return ()
 
+
+
 -- | Post data with curl
-post :: String -> Maybe String -> String -> Maybe String -> IO CurlCode
-post filetype id text desc = do
+post :: Options -> String -> IO CurlCode
+post opts text = do
     curl <- initialize
     mapM_ (setopt curl) [ CurlURL "http://npaste.de"
                         , CurlPost True 
                         , CurlPostFields $ [ "content="     ++ escape text
-                                           , "filetype="    ++ escape filetype
-                                           , "id="          ++ escape (fromMaybe "" id)
-                                           , "description=" ++ escape (fromMaybe "" desc)
+                                           , "filetype="    ++ escape (fromMaybe "" $ filetype opts)
+                                           , "id="          ++ escape (fromMaybe "" $ pId opts)
+                                           , "description=" ++ escape (fromMaybe "" $ description opts)
+                                           , "user="        ++ escape (fromMaybe "" $ user opts)
+                                           , "password="    ++ escape (fromMaybe "" $ password opts)
                                            ]
                         ]
     perform curl
@@ -71,6 +90,8 @@ data Options = Options { filetype :: Maybe String
                        , pId :: Maybe String
                        , description :: Maybe String
                        , replies :: [String]
+                       , user :: Maybe String
+                       , password :: Maybe String
                        }
 
 -- | Define options
@@ -81,15 +102,23 @@ options = [ Option "f" ["filetype"]
           , Option "i" ["id"]
                 (ReqArg (\arg opt -> return opt { pId = Just arg })
                         "ID")
-                "Define random or custom IDs. Use 'random' or 'rand' for random ID, anything else for custom ID."
+                "Define random or custom IDs. Use 'random' or 'rand' for random ID, anything else for custom ID"
           , Option "d" ["description"]
                 (ReqArg (\arg opt -> return opt { description = Just arg}) "DESCRIPTION")
-                "Add a description to your paste."
+                "Add a description to your paste"
           , Option "r" ["reply"]
                 (ReqArg (\arg opt -> return opt { replies = insert arg $ replies opt }) "ID")
-                "Reply to a paste with ID."
+                "Reply to a paste with ID"
           , Option "h" ["help"]
-                (NoArg (const $ do hPutStrLn stderr $ usageInfo "npaste.de client\n\nUseage:" options
+                (NoArg (const $ do hPutStrLn stderr $ usageInfo "npaste.de client v0.1\n\nUseage:" options
                                    exitWith ExitSuccess))
                 "Show help"
+
+          , Option "u" ["user"]
+                (ReqArg (\arg opt -> return opt { user = Just arg }) "USERNAME")
+                "Paste with your username"
+          , Option "p" ["password"]
+                (ReqArg (\arg opt -> return opt { password = Just arg }) "PASSWORD")
+                "Use your password with your username"
+
           ]
