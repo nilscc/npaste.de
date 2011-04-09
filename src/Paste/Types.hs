9f58751a08b211a164737ee4081379fead13431b
Nils Schweinsberg <mail@n-sch.de>
2011-04-09 16:50:02 +0200

    remove unnecessary files for npaste-state lib only

--- src/Paste/Types.hs
+++
@@ -1,9 +0,0 @@
-module Paste.Types
-    ( module Paste.Types.NewPaste
-    , module Paste.Types.Status
-    , module Paste.Types.Register
-    ) where
-
-import Paste.Types.NewPaste
-import Paste.Types.Status
-import Paste.Types.Register
