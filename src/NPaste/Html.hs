72fa83c0e3966d1c8de2a5edb2040f07cc11a6c6
Nils Schweinsberg <mail@n-sch.de>
2011-12-20 23:04:43 +0100

    split up about page, remove disclaimer

--- src/NPaste/Html.hs
+++ src/NPaste/Html.hs
@@ -1,10 +1,12 @@
 module NPaste.Html
   ( module NPaste.Html.Frames
+  , module NPaste.Html.About
   , module NPaste.Html.Index
   , module NPaste.Html.Read
   , module NPaste.Html.View
   ) where
 
+import NPaste.Html.About
 import NPaste.Html.Frames
 import NPaste.Html.Index
 import NPaste.Html.Read
--- src/NPaste/Routes.hs
+++ src/NPaste/Routes.hs
@@ -4,24 +4,21 @@ module NPaste.Routes
   ( npasteR
   ) where
 
-import Data.Time
 import Happstack.Server
 
 import qualified Text.Blaze.Html5             as H
 import qualified Text.Blaze.Html5.Attributes  as A
 import Text.Blaze ((!))
 
-import NPaste.Database
 import NPaste.Types
 import NPaste.State
 
+import NPaste.Routes.About
 import NPaste.Routes.Index
 import NPaste.Routes.Partial
 import NPaste.Routes.Read
 import NPaste.Routes.Static
 import NPaste.Routes.View
-
-import NPaste.Html.About
 
 npasteR :: NPaste ()
 npasteR = choice
@@ -43,13 +40,3 @@ notFoundR = do
   ResponseCode .= notFound
   Title        .= Just "Page not found"
   HtmlBody     .= H.h2 ! A.class_ "error" $ "The page you requested was not found."
-
-aboutR :: NPaste ()
-aboutR = do
-  setNP M_About
-  CSS      .= ["document.css"]
-  Title    .= Just "About"
-  u        <- getNumberOfUsers
-  p        <- getNumberOfPastes
-  now      <- liftIO getCurrentTime
-  HtmlBody .= aboutHtml u p now
