0dc8c21ce93d3babfeae5bdd9475f7b892836f97
Nils Schweinsberg <mail@n-sch.de>
2011-12-24 16:41:22 +0100

    remove debug stuff

--- src/NPaste/Utils/Happstack.hs
+++ src/NPaste/Utils/Happstack.hs
@@ -11,21 +11,17 @@ import NPaste.Types
 getPostData :: NPaste PostData
 getPostData = choice
   [ do methodM POST
-       liftIO $ putStrLn $ "getPostData called: lookPairs" -- debug
        _ <- optional $ decodeBody (defaultBodyPolicy "/tmp/npaste.de/" 1000000 1000000 1000000)
        fmap PostData $ body lookPairs
-  , do liftIO $ putStrLn $ "getPostData called: nullPostData" -- debug
-       return nullPostData
+  , return nullPostData
   ]
 
 getPostData' :: NPaste PostData
 getPostData' = choice
   [ do method POST
-       liftIO $ putStrLn $ "getPostData called: lookPairs" -- debug
        _ <- optional $ decodeBody (defaultBodyPolicy "/tmp/npaste.de/" 1000000 1000000 1000000)
        fmap PostData $ body lookPairs
-  , do liftIO $ putStrLn $ "getPostData called: nullPostData" -- debug
-       return nullPostData
+  , return nullPostData
   ]
 
 nullPostData :: PostData
