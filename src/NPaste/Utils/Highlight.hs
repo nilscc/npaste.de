22f26d5f26bd648976415834db3048b13484f64a
Nils Schweinsberg <mail@n-sch.de>
2012-01-23 16:05:02 +0100

    use own highlighter with utf8 decoding error handler

--- src/NPaste/Utils/Highlight.hs
+++ src/NPaste/Utils/Highlight.hs
@@ -7,13 +7,16 @@ module NPaste.Utils.Highlight
 import Data.ByteString (ByteString)
 import Data.Char
 import Data.List
+import Data.Text.Encoding (decodeUtf8With)
+import Data.Text.Encoding.Error (ignore)
 
 import Control.Applicative
 import Control.Monad
 
 import Text.Blaze
+import qualified Text.Blaze.Html5             as H
+import qualified Text.Blaze.Html5.Attributes  as A
 import Text.Highlighter
-import Text.Highlighter.Formatters.Html
 
 highlightAs :: String       -- ^ language
             -> ByteString   -- ^ source
@@ -21,9 +24,17 @@ highlightAs :: String       -- ^ language
 highlightAs lang src = do -- maybe monad
   lexr <- findLexer lang
   toks <- maybeLeft (runLexer lexr src)
-  Just (format False toks)
+  Just (highlight toks)
  where
   maybeLeft = either (const Nothing) Just
+
+-- stolen from Text.Highlighter.Formatters.Html :)
+highlight :: [Token] -> Html
+highlight [] = return ()
+highlight (Token t s:ts) = do
+  H.span ! A.class_ (toValue $ shortName t) $
+    toHtml (decodeUtf8With ignore s)
+  highlight ts
 
 findLexer :: String -> Maybe Lexer
 findLexer lang =
